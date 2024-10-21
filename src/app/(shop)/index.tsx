151cd49b9a39ae908b2c05efeb9c914316f5264f
Jesus Chacon <jesuszen2.6@gmail.com>
2024-10-20 23:52:25 -0400

    feat: integrate assets image and types base and integrate home page

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,11 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+
+import { PRODUCTS } from '../../../assets/products'
+import {ProductListItem} from '../../components/product-list-item'
+import { ListHeader } from '../../components/list-header'
 
 const index = () => {
   return (
     <View>
-      <Text>index</Text>
+      <FlatList
+      data={PRODUCTS}
+      renderItem={({ item }) => (
+        <ProductListItem product={item} />
+      )} 
+      numColumns={2}
+      ListHeaderComponent={<ListHeader />}
+      contentContainerStyle={style.flatListContent}
+      columnWrapperStyle={style.flatListColumn}
+      style={{paddingHorizontal: 10, paddingVertical: 5}}
+      keyExtractor={item => item.id.toString()}
+      />
     </View>
   )
 }
 
 export default index
+
+const style = StyleSheet.create({
+  flatListContent: {
+    paddingBottom: 20,
+  },
+  flatListColumn: {
+    justifyContent: 'space-between',
+  }
+})
